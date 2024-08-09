e3b709462fd2a02651c718f3ea6a4299a96a821f
Aman Malik <100064294+AmanMalik12@users.noreply.github.com>
2024-08-09 23:41:20 +0530

    Add files via upload

---
+++ components/Footer.php
@@ -0,0 +1,8 @@
+<footer>
+    <div class="fwrapper">
+        <img src="images/logo.png" alt="Unable to load logo">
+        <a href="mailto:voltsretailer@gmail.com">voltsretail@gmail.com</a>
+        <span>&copy;2022 by Volt Retail. Proudly created with</span>
+        <span>LightHouse</span>
+    </div>
+</footer>
---
+++ components/Navbar.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-default navbar-expand-lg navbar-light" style="background: #f8f8f8;">
+	<div class="navbar-header">
+		<a href="index.php"><img src="images/logo.png" height="50" width="180" ></a>  		
+		<button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle">
+			<span class="navbar-toggler-icon"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+		</button>
+	</div>
+	<div id="navbarCollapse" class="collapse navbar-collapse">
+		<ul class="nav navbar-nav">
+			<li><a href="index.php">Home</a></li>
+			<li><a href="page.php?id=shop">Shop</a></li>
+		</ul>
+		<!-- <ul class="nav navbar-nav navbar-right">			
+			<li>
+				<a href="#">Login</a>
+			</li>
+			<li>
+				<a href="#" class="btn btn-warning get-started-btn mt-1 mb-1" style="padding: 8px 10px;">Sign up</a>
+			</li>
+		</ul> -->
+</nav>
